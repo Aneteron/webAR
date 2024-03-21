dc484d6a9cfc418b41ae512961ca728fe2aa5983
Aneteron <99216347+Aneteron@users.noreply.github.com>
2024-03-21 17:20:50 +0200

    0.0.2

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 console.log("My app .js");
 import * as THREE from "./libs/three/three.module.js";
 console.log("THREE", THREE);
+import { OrbitControls } from "./libs/three/jsm/OrbitControls.js";
+console.log("Orbit controll", OrbitControls);
+
+class App {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {}
+
+  render() {}
+}
+
+export { App };
