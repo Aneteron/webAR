2880b771daf81846292dd08f7e274659c9d28bd9
Aneteron <99216347+Aneteron@users.noreply.github.com>
2024-03-21 18:03:10 +0200

    0.0.3

--- app.js
+++ app.js
@@ -18,24 +18,48 @@ class App {
     );
     this.camera.position.set(0, 0, 4);
 
-    //Scene
+    // Scene
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xaaaaaa);
 
-    //Renderer
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    // // Light
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+    this.scene.add(ambient);
+
+    const light = new THREE.DirectionalLight();
+    light.position.set(0.2, 1, 1);
+    this.scene.add(light);
+
+    // Renderer
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(this.renderer.domElement);
+
+    //Geometry
+
+    const geometry = new THREE.TetrahedronBufferGeometry();
+    const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.mesh);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     this.renderer.setAnimationLoop(this.render.bind(this));
 
     window.addEventListener("resize", this.resize.bind(this));
   }
 
-  resize() {}
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
   render() {
+    this.mesh.rotateY(0.01);
     this.renderer.render(this.scene, this.camera);
   }
 }
